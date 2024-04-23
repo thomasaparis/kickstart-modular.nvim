8a276b8b0884471f6a1967517ed91a011a481aac
thomasaparis <25939513+thomasaparis@users.noreply.github.com>
2024-04-23 14:29:04 -0700

    +alpha, -vircolumn

--- lua/custom/plugins/virtcolumn-nvim.lua
+++
@@ -1,7 +0,0 @@
-return {
-  'xiyaowong/virtcolumn.nvim',
-  config = function()
-    vim.g.virtcolumn_char = '▕' -- char to display the line
-    vim.g.virtcolumn_priority = 10 -- priority of extmark
-  end,
-}
