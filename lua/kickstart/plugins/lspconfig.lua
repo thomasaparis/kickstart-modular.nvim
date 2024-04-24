ff699ccaf8e354c1f1dc465750aa13e3aef56f21
thomasaparis <25939513+thomasaparis@users.noreply.github.com>
2024-04-24 16:31:49 -0700

    Update lspconfig.lua

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -137,7 +137,8 @@ return {
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
+        elixirls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
