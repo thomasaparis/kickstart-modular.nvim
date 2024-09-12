ce6fe7bc09d5cce9df48e2dc87eb5333a15682a2
thomasaparis <25939513+thomasaparis@users.noreply.github.com>
2024-09-12 12:30:27 -0700

    Create harpoon.lua

---
+++ lua/custom/plugins/harpoon.lua
@@ -0,0 +1,16 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    vim.keymap.set('n', 'hx', require('harpoon.mark').add_file)
+    vim.keymap.set('n', 'hn', require('harpoon.ui').nav_next)
+    vim.keymap.set('n', 'hp', require('harpoon.ui').nav_prev)
+    vim.keymap.set('n', '<leader>h', require('harpoon.ui').toggle_quick_menu, { desc = '[H]arpoon toggle' })
+    require('harpoon').setup {
+      global_settings = {
+        mark_branch = true,
+      },
+    }
+  end,
+}
