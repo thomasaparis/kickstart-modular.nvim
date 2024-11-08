98903f7f0882b3ebc7be9e17333caa709f089a44
thomasaparis <25939513+thomasaparis@users.noreply.github.com>
2024-11-08 12:31:56 -0800

    Update conform.lua

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -3,18 +3,41 @@ return {
     'stevearc/conform.nvim',
     lazy = false,
     keys = {
+      -- {
+      --   '<leader>f',
+      --   function()
+      --     require('conform').format { async = true, lsp_fallback = true }
+      --   end,
+      --   mode = '',
+      --   desc = '[F]ormat buffer',
+      -- },
       {
-        '<leader>f',
+        '<leader>ft',
         function()
-          require('conform').format { async = true, lsp_fallback = true }
+          local conform = require 'conform'
+          if vim.g.disable_autoformat then
+            vim.g.disable_autoformat = false
+            vim.notify('Autoformat enabled', vim.log.levels.INFO)
+          else
+            vim.g.disable_autoformat = true
+            vim.notify('Autoformat disabled', vim.log.levels.INFO)
+          end
         end,
-        mode = '',
-        desc = '[F]ormat buffer',
+        mode = 'n',
+        desc = 'Toggle format on save',
       },
     },
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
+        -- Check if autoformat is disabled globally
+        if vim.g.disable_autoformat then
+          return
+        end
+        -- Check if autoformat is disabled for this buffer
+        if vim.b[bufnr].disable_autoformat then
+          return
+        end
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
@@ -37,6 +60,33 @@ return {
         -- javascript = { { 'prettierd', 'prettier' } },
       },
     },
+    config = function(_, opts)
+      local conform = require 'conform'
+
+      conform.setup(opts)
+
+      vim.api.nvim_create_user_command('FormatDisable', function(args)
+        if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+          vim.notify('Autoformat disabled for this buffer', vim.log.levels.INFO)
+        else
+          vim.g.disable_autoformat = true
+          vim.notify('Autoformat disabled globally', vim.log.levels.INFO)
+        end
+      end, {
+        desc = 'Disable autoformat-on-save',
+        bang = true,
+      })
+
+      vim.api.nvim_create_user_command('FormatEnable', function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+        vim.notify('Autoformat enabled', vim.log.levels.INFO)
+      end, {
+        desc = 'Re-enable autoformat-on-save',
+      })
+    end,
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
