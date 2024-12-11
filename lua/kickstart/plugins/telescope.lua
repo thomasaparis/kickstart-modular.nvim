426178a4987cb9a4abb481f30d8412e48d310992
thomasaparis <25939513+thomasaparis@users.noreply.github.com>
2024-12-11 13:02:38 -0800

    Update telescope current_buffer_search appearance

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -105,10 +105,10 @@ return {
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to telescope to change theme, layout, etc.
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          winblend = 10,
-          previewer = false,
-        })
+        -- builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+        --   winblend = 10,
+        --   previewer = false,
+        builtin.current_buffer_fuzzy_find()
       end, { desc = '[/] Fuzzily search in current buffer' })
 
       -- Also possible to pass additional configuration options.
