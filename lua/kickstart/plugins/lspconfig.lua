858ae754ee25237d29ee94721f7a7681eab49c51
thomasaparis <25939513+thomasaparis@users.noreply.github.com>
2025-01-06 10:17:55 -0800

    lspconfig merge work and home

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -138,7 +138,7 @@ return {
         -- clangd = {},
         -- gopls = {},
         pyright = {},
-        elixirls = {},
+        -- elixirls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
