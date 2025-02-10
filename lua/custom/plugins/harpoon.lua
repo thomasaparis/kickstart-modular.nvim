990c71d662cd3167a586fcae52ad7f9412b9f7d6
thomasaparis <25939513+thomasaparis@users.noreply.github.com>
2025-02-10 12:55:38 -0800

    Update harpoon keybinds

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -2,9 +2,9 @@ return {
   'ThePrimeagen/harpoon',
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
-    vim.keymap.set('n', 'hx', require('harpoon.mark').add_file)
-    vim.keymap.set('n', 'hn', require('harpoon.ui').nav_next)
-    vim.keymap.set('n', 'hp', require('harpoon.ui').nav_prev)
+    vim.keymap.set('n', '<leader>hx', require('harpoon.mark').add_file)
+    vim.keymap.set('n', '<leader>hn', require('harpoon.ui').nav_next)
+    vim.keymap.set('n', '<leader>hp', require('harpoon.ui').nav_prev)
     vim.keymap.set('n', '<leader>th', require('harpoon.ui').toggle_quick_menu, { desc = '[T]oggle [H]arpoon' })
     require('harpoon').setup {
       global_settings = {
