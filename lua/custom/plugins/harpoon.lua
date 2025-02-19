dcf8afe75d99f7658dcf9c5aeacf2304a8899305
thomasaparis <25939513+thomasaparis@users.noreply.github.com>
2025-02-19 10:38:07 -0800

    Fix harpoon keybind collision

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -5,7 +5,7 @@ return {
     vim.keymap.set('n', '<leader>hx', require('harpoon.mark').add_file)
     vim.keymap.set('n', '<leader>hn', require('harpoon.ui').nav_next)
     vim.keymap.set('n', '<leader>hp', require('harpoon.ui').nav_prev)
-    vim.keymap.set('n', '<leader>th', require('harpoon.ui').toggle_quick_menu, { desc = '[T]oggle [H]arpoon' })
+    vim.keymap.set('n', '<leader>thp', require('harpoon.ui').toggle_quick_menu, { desc = '[T]oggle [H]ar[p]oon' })
     require('harpoon').setup {
       global_settings = {
         mark_branch = true,
